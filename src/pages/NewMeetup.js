44fb5a6525fc073dc25d5642ea1d9e61b061b507
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-02-18 19:56:44 +0100

    get meetupData and send it to firebase realtime database through POST request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,9 +3,23 @@ import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 
 function NewMeetup() {
+
+    const url = 'https://meetup-project-react-8d99a-default-rtdb.europe-west1.firebasedatabase.app/meetups.json';
+
+    const handleAddMeetup = (meetupData) => {
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
     return (
         <div>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={handleAddMeetup} />
         </div>
     );
 }
