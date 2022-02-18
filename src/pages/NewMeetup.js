bd86e37ffdd7ad4a49ca1a6ff962a44d8ba6f4b0
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-02-18 17:44:50 +0100

    init input form in NewMeetupForm component

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 
 function NewMeetup() {
     return (
         <div>
-            NewMeetup
+            <NewMeetupForm />
         </div>
     );
 }
